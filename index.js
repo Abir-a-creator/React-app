dccc1c2402be5534e3c574050e0d192fdcabdf57
Abir-a-creator <72654344+Abir-a-creator@users.noreply.github.com>
2021-01-08 16:45:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const myfirstelement = <h1>Welcome to our book club</h1>
+
+ReactDOM.render(myfirstelement, document.getElementById('root'));
